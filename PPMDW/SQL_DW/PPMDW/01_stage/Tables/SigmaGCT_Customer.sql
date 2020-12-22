44f40336d38cb3ca90b7bb8154354c60236be105
sherlockspreadsheets <brian.lofton@georgesinc.com>
2020-12-22 16:51:34 -0600

    Sigma tables added to stage

---
+++ PPMDW/SQL_DW/PPMDW/01_stage/Tables/SigmaGCT_Customer.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[SigmaGCT_Customer](
+	[CustomerID] [int] NOT NULL,
+	[MfgID] [nvarchar](7) NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](75) NULL,
+	[CodeValue] [int] NULL,
+	[CodeText] [nvarchar](50) NULL,
+	[Comment] [nvarchar](200) NULL,
+	[Sequence] [int] NULL,
+	[CreatedTime] [datetime] NULL,
+	[CreatedByUserID] [int] NULL,
+	[LastModified] [datetime] NULL,
+	[LastUserID] [int] NULL,
+	[IsReserved] [bit] NULL,
+	[IsInactive] [bit] NULL,
+	[refStatusID] [int] NULL,
+	[refTypeID] [int] NULL,
+	[refCategoryID] [int] NULL
+) ON [PRIMARY]
